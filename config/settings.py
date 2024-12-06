f8fcefeadd346d58fd4e61519d03e537f9a50841
sarvar <sarvaruzb01@gmail.com>
2024-12-06 17:42:45 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = int(os.getenv('DEBUG', 1))
 
 ALLOWED_HOSTS = ['*']
 
